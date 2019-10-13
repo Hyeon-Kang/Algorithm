c255a2e2e29bb33553fdce05f885747c205d3ea5
Hyeon-Kang <44962939+Hyeon-Kang@users.noreply.github.com>
2019-10-13 20:24:41 +0900

    Update BTree.java

--- week_04/BTree.java
+++ week_04/BTree.java
@@ -107,19 +107,22 @@ public class BTree {
 		}
 		
 		while(stack.isEmpty() != true) { // 공백 스택이 아닐 때
-			if(p != null) { // p 역시 노드가 아닌 경우
-				p = stack.peek(); // 스택에서 맨 위에 있는 데이터 읽기
+			if(p != null) { // p 역시 공백 노드가 아닌 경우
+				p = stack.peek(); // 스택에서 맨 위에 있는 데이터 읽기 (루트부터 접근)
 				
 				p = p.left; // p 의 초점을 좌측 서브노드로 이동
 				while(p != null) { // 왼쪽 트리 순회
 					stack.push(p); // -> |p|p.left|
-					//System.out.println("추적");
-					p = p.left;
+					p = p.left; // 좌측 서브노드를 탐색하기 위해 이동 
+					// (다음 순환에서 p=p.left 가 null로 판정된 경우 stack에 들은 |p.left|의 데이터를 읽고 탈출
+					
 				}
 			}
 			
 			p = stack.pop(); // 스택의 좌측트리 데이터 pop  -> |p|
 			System.out.print(p.item + " ");
+			
+			// 우측 데이터
 			p = p.right;
 			if(p != null) { 
 				stack.push(p); 
@@ -145,6 +148,40 @@ public class BTree {
 		}
 		
 	}
+	
+	
+	// root.item -> root.left -> root.right
+	public void iterPreorder() {
+		Stack<TreeNode> stack = new Stack<TreeNode>(); // 스택 초기화
+		TreeNode p = root; // 임시노드 p에 this.root 복사
+		
+		// root node부터 접근
+		if (p != null) {
+			stack.push(p); // 스택에 루트 노드 저장
+		}
+		
+		// 공백 스택이 아닌경우
+		while (stack.isEmpty() != true) {
+			
+			
+			// root 부터 접근해야 하므로 들어있는 데이터 꺼내기
+			p=stack.pop();
+			System.out.print(p.item + " ");
+
+			// 스택이 FIFO 특성임을 감안하여 오른쪽 자식부터 집어넣는다. 
+			// (꺼낼 때는 나중에 넣은 왼쪽부터 나오게!) 
+			if (p.right!=null) {
+				stack.push(p.right);
+			}
+			
+			// 우측 서브노드
+			if (p.left!=null) {
+				stack.push(p.left);
+			}
+		}
+
+	}
+
 
 
 }
